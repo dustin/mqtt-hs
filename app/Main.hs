18dff9b4162dc2a4cd8f0eb03327dc51cf41e6e1
Dustin Sallings <dustin@spy.net>
2019-01-02 10:41:57 -0800

    Trying out a LWT.  Not ideal yet.

--- app/Main.hs
+++ app/Main.hs
@@ -7,10 +7,16 @@ import           Control.Monad       (forever)
 import           Data.Default        (def)
 
 import           Network.MQTT.Client
+import qualified Network.MQTT.Types  as MT
 
 main :: IO ()
 main = do
-  mc <- connectTo def{_hostname="localhost", _service="1883", _connID="hasqttl", _msgCB=Just showme}
+  mc <- connectTo def{_hostname="localhost", _service="1883", _connID="hasqttl",
+                      _lwt=Just MT.LastWill{MT._willRetain=False,
+                                            MT._willQoS=0,
+                                            MT._willTopic="tmp/haskquit",
+                                            MT._willMsg="bye for now"},
+                      _msgCB=Just showme}
   subscribe mc [("oro/#", 0), ("tmp/#", 0)]
   forever $ publish mc "tmp/mqtths" "hi from haskell" False >> threadDelay 10000000
 
