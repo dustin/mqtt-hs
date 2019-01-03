2a52d1e5acd2dcc5c8a51d8dce8ef32db8be2c74
Dustin Sallings <dustin@spy.net>
2019-01-03 09:56:34 -0800

    Manage subscribe and unsubscribe responses

--- src/Network/MQTT/Client.hs
+++ src/Network/MQTT/Client.hs
@@ -9,12 +9,14 @@ import           Control.Concurrent.Async        (Async, async, cancel,
 import           Control.Concurrent.STM          (TChan, TVar, atomically,
                                                   modifyTVar', newTChanIO,
                                                   newTVarIO, readTChan,
-                                                  readTVar, writeTChan)
+                                                  readTVar, retry, writeTChan)
 import qualified Control.Exception               as E
 import           Control.Monad                   (forever)
 import qualified Data.Attoparsec.ByteString.Lazy as A
 import qualified Data.ByteString.Lazy            as BL
 import qualified Data.ByteString.Lazy.Char8      as BC
+import           Data.IntMap                     (IntMap)
+import qualified Data.IntMap                     as IntMap
 import           Data.Text                       (Text)
 import qualified Data.Text.Encoding              as TE
 import           Data.Word                       (Word16)
@@ -35,6 +37,7 @@ data MQTTClient = MQTTClient {
   , _pktID :: TVar Word16
   , _cb    :: Maybe (Text -> BL.ByteString -> IO ())
   , _ts    :: TVar [Async ()]
+  , _acks  :: TVar (IntMap MQTTPkt)
   }
 
 data MQTTConfig = MQTTConfig{
@@ -59,12 +62,14 @@ runClient MQTTConfig{..} = do
   ch <- newTChanIO
   pid <- newTVarIO 0
   thr <- newTVarIO []
+  acks <- newTVarIO mempty
   let cli = MQTTClient{_out=undefined,
                        _in=mempty,
                        _ch=ch,
                        _cb=_msgCB,
                        _pktID=pid,
-                       _ts=thr}
+                       _ts=thr,
+                       _acks=acks}
 
   t <- async $ clientThread cli
   atomically $ modifyTVar' (_ts cli) (t:)
@@ -126,8 +131,12 @@ capture c@MQTTClient{..} = do
     (PublishPkt PublishRequest{..}) -> case _cb of
                                          Nothing -> pure ()
                                          Just x -> x (blToText _pubTopic) _pubBody
+    (SubACKPkt (SubscribeResponse i _)) -> remember res i
+    (UnsubACKPkt (UnsubscribeResponse i)) -> remember res i
     x -> print x
   capture c{_in=s'}
+
+  where remember pkt pid = atomically $ modifyTVar' _acks (\m -> IntMap.insert (fromEnum pid) pkt m)
 
 sendPacket :: MQTTClient -> MQTTPkt -> IO ()
 sendPacket MQTTClient{..} = atomically . writeTChan _ch
@@ -138,13 +147,44 @@ textToBL = BL.fromStrict . TE.encodeUtf8
 blToText :: BL.ByteString -> Text
 blToText = TE.decodeUtf8 . BL.toStrict
 
-subscribe :: MQTTClient -> [(Text, Int)] -> IO ()
-subscribe MQTTClient{..} ls = atomically $ do
-  pid <- readTVar _pktID
-  modifyTVar' _pktID succ
-  writeTChan _ch (SubscribePkt $ SubscribeRequest pid ls')
+subscribe :: MQTTClient -> [(Text, Int)] -> IO [Int]
+subscribe MQTTClient{..} ls = do
+  p <- atomically $ do
+    pid <- readTVar _pktID
+    modifyTVar' _pktID succ
+    writeTChan _ch (SubscribePkt $ SubscribeRequest pid ls')
+    pure pid
+
+  let pint = fromEnum p
+  r <- atomically $ do
+    m <- readTVar _acks
+    case IntMap.lookup pint m of
+      Nothing  -> retry
+      Just pkt -> modifyTVar' _acks (IntMap.delete pint) >> pure pkt
+
+  let (SubACKPkt (SubscribeResponse _ rs)) = r
+  pure $ map fromEnum rs
 
     where ls' = map (\(s, i) -> (textToBL s, toEnum i)) ls
+
+unsubscribe :: MQTTClient -> [Text] -> IO ()
+unsubscribe MQTTClient{..} ls = do
+  p <- atomically $ do
+    pid <- readTVar _pktID
+    modifyTVar' _pktID succ
+    writeTChan _ch (UnsubscribePkt $ UnsubscribeRequest pid ls')
+    pure pid
+
+  let pint = fromEnum p
+  atomically $ do
+    m <- readTVar _acks
+    case IntMap.lookup pint m of
+      Nothing -> retry
+      Just _  -> modifyTVar' _acks (IntMap.delete pint)
+
+  pure ()
+
+    where ls' = map textToBL ls
 
 publish :: MQTTClient -> Text -> BL.ByteString -> Bool -> IO ()
 publish c t m r = sendPacket c (PublishPkt $ PublishRequest {
