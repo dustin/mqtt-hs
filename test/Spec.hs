e316b01cb6adee8086f3f459a572c4d54e5c2457
Dustin Sallings <dustin@spy.net>
2019-09-17 18:39:03 -0700

    Separate Arbitrary out of tests so external packages can use them.

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,29 +1,17 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards   #-}
 
-import           Control.Applicative             (liftA2)
 import           Control.Monad                   (mapM_)
 import qualified Data.Attoparsec.ByteString.Lazy as A
-import qualified Data.ByteString                 as B
-import qualified Data.ByteString.Char8           as BC
 import qualified Data.ByteString.Lazy            as L
 import           Data.Word                       (Word8)
-import           Numeric                         (showHex)
+import           Network.MQTT.Arbitrary
+import           Network.MQTT.Topic
+import           Network.MQTT.Types              as MT
 import           Test.QuickCheck                 as QC
 import           Test.Tasty
 import           Test.Tasty.HUnit
 import           Test.Tasty.QuickCheck           as QC
-
-import           Network.MQTT.Topic
-import           Network.MQTT.Types              as MT
-
-newtype SizeT = SizeT Int deriving(Eq, Show)
-
-instance Arbitrary SizeT where
-  arbitrary = SizeT <$> oneof [ choose (0, 127),
-                                choose (128, 16383),
-                                choose (16384, 2097151),
-                                choose (2097152, 268435455)]
 
 prop_rtLengthParser :: SizeT -> QC.Property
 prop_rtLengthParser (SizeT x) =
@@ -48,157 +36,6 @@ testPacketRT = mapM_ tryParse [
       case A.parse (parsePacket Protocol311) (toByteString Protocol311 x) of
         f@A.Fail{}    -> assertFailure (show f)
         (A.Done _ x') -> assertEqual (show s) x x'
-
-instance Arbitrary LastWill where
-  arbitrary = LastWill <$> arbitrary <*> arbitrary <*> astr <*> astr <*> arbitrary
-
-instance Arbitrary ProtocolLevel where arbitrary = arbitraryBoundedEnum
-
-instance Arbitrary ConnectRequest where
-  arbitrary = do
-    _username <- mastr
-    _password <- mastr
-    _connID <- astr
-    _cleanSession <- arbitrary
-    _keepAlive <- arbitrary
-    _lastWill <- arbitrary
-    _properties <- arbitrary
-
-    pure ConnectRequest{..}
-
-mastr :: Gen (Maybe L.ByteString)
-mastr = fmap (L.fromStrict . BC.pack . getUnicodeString) <$> arbitrary
-
-astr :: Gen L.ByteString
-astr = L.fromStrict . BC.pack . getUnicodeString <$> arbitrary
-
-instance Arbitrary QoS where
-  arbitrary = arbitraryBoundedEnum
-
-instance Arbitrary ConnACKFlags where
-  arbitrary = ConnACKFlags <$> arbitrary <*> arbitrary <*> arbitrary
-  shrink (ConnACKFlags b c pl)
-    | null pl = []
-    | otherwise = ConnACKFlags b c <$> shrink pl
-
-instance Arbitrary PublishRequest where
-  arbitrary = do
-    _pubDup <- arbitrary
-    _pubQoS <- arbitrary
-    _pubRetain <- arbitrary
-    _pubTopic <- astr
-    _pubPktID <- if _pubQoS == QoS0 then pure 0 else arbitrary
-    _pubBody <- astr
-    _pubProps <- arbitrary
-    pure PublishRequest{..}
-
-instance Arbitrary PubACK where
-  arbitrary = PubACK <$> arbitrary <*> arbitrary <*> arbitrary
-
-instance Arbitrary PubREL where
-  arbitrary = PubREL <$> arbitrary <*> arbitrary <*> arbitrary
-
-instance Arbitrary PubREC where
-  arbitrary = PubREC <$> arbitrary <*> arbitrary <*> arbitrary
-
-instance Arbitrary PubCOMP where
-  arbitrary = PubCOMP <$> arbitrary <*> arbitrary <*> arbitrary
-
-instance Arbitrary SubscribeRequest where
-  arbitrary = arbitrary >>= \pid -> choose (1,11) >>= \n -> SubscribeRequest pid <$> vectorOf n sub <*> arbitrary
-    where sub = liftA2 (,) astr arbitrary
-
-  shrink (SubscribeRequest w s p) =
-    if length s < 2 then []
-    else [SubscribeRequest w (take 1 s) p' | p' <- shrinkList (:[]) p, length p > 1]
-
-instance Arbitrary SubOptions where
-  arbitrary = SubOptions <$> arbitraryBoundedEnum <*> arbitrary <*> arbitrary <*> arbitrary
-
-instance Arbitrary SubErr where arbitrary = arbitraryBoundedEnum
-
-instance Arbitrary SubscribeResponse where
-  arbitrary = arbitrary >>= \pid -> choose (1,11) >>= \n -> SubscribeResponse pid <$> vectorOf n arbitrary <*> arbitrary
-
-  shrink (SubscribeResponse pid l props)
-    | length l == 1 = []
-    | otherwise = [SubscribeResponse pid sl props | sl <- shrinkList (:[]) l, not (null sl)]
-
-instance Arbitrary UnsubscribeRequest where
-  arbitrary = arbitrary >>= \pid -> choose (1,11) >>= \n -> UnsubscribeRequest pid <$> vectorOf n astr <*> arbitrary
-  shrink (UnsubscribeRequest p l props)
-    | length l == 1 = []
-    | otherwise = [UnsubscribeRequest p sl props | sl <- shrinkList (:[]) l, not (null sl)]
-
-instance Arbitrary UnsubStatus where arbitrary = arbitraryBoundedEnum
-
-instance Arbitrary UnsubscribeResponse where
-  arbitrary = UnsubscribeResponse <$> arbitrary <*> arbitrary <*> arbitrary
-
-instance Arbitrary MT.Property where
-  arbitrary = oneof [
-    PropPayloadFormatIndicator <$> arbitrary,
-    PropMessageExpiryInterval <$> arbitrary,
-    PropMessageExpiryInterval <$> arbitrary,
-    PropContentType <$> astr,
-    PropResponseTopic <$> astr,
-    PropCorrelationData <$> astr,
-    PropSubscriptionIdentifier <$> arbitrary `suchThat` (>= 0),
-    PropSessionExpiryInterval <$> arbitrary,
-    PropAssignedClientIdentifier <$> astr,
-    PropServerKeepAlive <$> arbitrary,
-    PropAuthenticationMethod <$> astr,
-    PropAuthenticationData <$> astr,
-    PropRequestProblemInformation <$> arbitrary,
-    PropWillDelayInterval <$> arbitrary,
-    PropRequestResponseInformation <$> arbitrary,
-    PropResponseInformation <$> astr,
-    PropServerReference <$> astr,
-    PropReasonString <$> astr,
-    PropReceiveMaximum <$> arbitrary,
-    PropTopicAliasMaximum <$> arbitrary,
-    PropTopicAlias <$> arbitrary,
-    PropMaximumQoS <$> arbitrary,
-    PropRetainAvailable <$> arbitrary,
-    PropUserProperty <$> astr <*> astr,
-    PropMaximumPacketSize <$> arbitrary,
-    PropWildcardSubscriptionAvailable <$> arbitrary,
-    PropSubscriptionIdentifierAvailable <$> arbitrary,
-    PropSharedSubscriptionAvailable <$> arbitrary
-    ]
-
-instance Arbitrary AuthRequest where
-  arbitrary = AuthRequest <$> arbitrary <*> arbitrary
-
-instance Arbitrary ConnACKRC where arbitrary = arbitraryBoundedEnum
-
-instance Arbitrary DiscoReason where arbitrary = arbitraryBoundedEnum
-
-instance Arbitrary DisconnectRequest where
-  arbitrary = DisconnectRequest <$> arbitrary <*> arbitrary
-
-instance Arbitrary MQTTPkt where
-  arbitrary = oneof [
-    ConnPkt <$> arbitrary,
-    ConnACKPkt <$> arbitrary,
-    PublishPkt <$> arbitrary,
-    PubACKPkt <$> arbitrary,
-    PubRELPkt <$> arbitrary,
-    PubRECPkt <$> arbitrary,
-    PubCOMPPkt <$> arbitrary,
-    SubscribePkt <$> arbitrary,
-    SubACKPkt <$> arbitrary,
-    UnsubscribePkt <$> arbitrary,
-    UnsubACKPkt <$> arbitrary,
-    pure PingPkt, pure PongPkt,
-    DisconnectPkt <$> arbitrary,
-    AuthPkt <$> arbitrary
-    ]
-  shrink (SubACKPkt x)      = SubACKPkt <$> shrink x
-  shrink (ConnACKPkt x)     = ConnACKPkt <$> shrink x
-  shrink (UnsubscribePkt x) = UnsubscribePkt <$> shrink x
-  shrink (SubscribePkt x)   = SubscribePkt <$> shrink x
-  shrink _                  = []
 
 prop_PacketRT50 :: MQTTPkt -> QC.Property
 prop_PacketRT50 p = label (lab p) $ case A.parse (parsePacket Protocol50) (toByteString Protocol50 p) of
