dcacabe33ba259e5ce1b5adb4cb1db50ad5223b1
Dustin Sallings <dustin@spy.net>
2023-04-08 10:30:11 -1000

    LTS and nix updates

--- shell.nix
+++ shell.nix
@@ -1,1 +1,17 @@
-(import ./default.nix {}).shell
+{ pkgs ? import <nixpkgs> { } }:
+with pkgs;
+mkShell {
+  buildInputs = [
+    stack
+    sqlite-interactive
+    ffmpeg
+    stylish-haskell
+    hlint
+    ghcid
+    haskell-language-server
+  ];
+
+  shellHook = ''
+    # ...
+  '';
+}
