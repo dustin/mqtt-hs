8cdf004b713934463d9fb66858f3c5f363db688f
Alberto Valverde <alberto@toscat.net>
2023-08-18 11:42:57 +0200

    auto cleanup corr and inflight maps

---
+++ src/Data/Map/Strict/Decaying.hs
@@ -0,0 +1,60 @@
+module Data.Map.Strict.Decaying
+  ( Map,
+    new,
+    insert,
+    delete,
+    findWithDefault,
+    updateLookupWithKey,
+  )
+where
+
+import Control.Concurrent.Async (async, cancel, link)
+import Control.Concurrent.STM (STM, atomically)
+import Control.Concurrent.STM.TVar
+  ( TVar,
+    mkWeakTVar,
+    modifyTVar',
+    newTVarIO,
+    readTVar,
+    writeTVar
+  )
+import Control.Exception (mask)
+import Control.Monad (forever, void)
+import qualified Data.Map.Strict as Map
+import Data.Time (NominalDiffTime)
+import Data.Time.Clock.POSIX (POSIXTime, getPOSIXTime)
+import GHC.Conc (threadDelay, unsafeIOToSTM)
+
+data Item a = Item {_itemTime :: {-# UNPACK #-} !POSIXTime, itemValue :: !a}
+
+newtype Map k a = Map (TVar (Map.Map k (Item a)))
+
+insert :: Ord k => k -> v -> Map k v -> STM ()
+insert k v (Map m) = do
+  t <- unsafeIOToSTM getPOSIXTime
+  modifyTVar' m (Map.insert k (Item t v))
+
+delete :: Ord k => k -> Map k v -> STM ()
+delete k (Map m) = modifyTVar' m (Map.delete k)
+
+findWithDefault :: Ord k => v -> k -> Map k v -> STM v
+findWithDefault d k (Map m) = itemValue . Map.findWithDefault (Item 0 d) k <$> readTVar m
+
+updateLookupWithKey ::
+  Ord k => (k -> a -> Maybe a) -> k -> Map k a -> STM (Maybe a)
+updateLookupWithKey f k (Map m) = do
+  t <- unsafeIOToSTM getPOSIXTime
+  (mItem, m') <- Map.updateLookupWithKey (\k -> fmap (Item t) . f k . itemValue) k <$> readTVar m
+  writeTVar m $! m'
+  pure (itemValue <$> mItem)
+
+new :: NominalDiffTime -> IO (Map k a)
+new maxAge = mask $ \restore -> do
+  var <- newTVarIO Map.empty
+  reaper <- restore $ async $ forever $ do
+    now <- getPOSIXTime
+    atomically $ modifyTVar' var $ Map.filter $ \(Item t _) -> now - t < maxAge
+    threadDelay $ truncate $ maxAge / 4
+  link reaper
+  void $ mkWeakTVar var $ cancel reaper
+  pure $ Map var
